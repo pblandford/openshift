4f52e1d5330e9ad86c3fbe9b3f2b2f9842cab57c
Philip Blandford <philip.blandford@gmail.com>
2014-10-22 13:11:14 +0100

    change settings path

--- CurrencyFeed/wsgi.py
+++ CurrencyFeed/wsgi.py
@@ -11,7 +11,7 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', ".."))
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CurrencyFeed.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
