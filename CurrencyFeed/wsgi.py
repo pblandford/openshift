e0b255238dd9cd198dd8d7f8a7cc4cee86e05cb6
Philip Blandford <philip.blandford@gmail.com>
2014-10-22 13:08:42 +0100

    move import os

--- CurrencyFeed/wsgi.py
+++ CurrencyFeed/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import sys
+import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', ".."))
 
-import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 from django.core.wsgi import get_wsgi_application
